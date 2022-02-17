043011554fae5bba6056a69426cebda09a8bc445
Anthony <anthonyharisson30@gmail.com>
2022-02-17 20:34:03 -0300

    Almost Done (no local

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const inputElement = document.querySelector(".new-task-input");
 const addTaskButton = document.querySelector(".new-task-button");
 
+const taskContainer = document.querySelector('.tasks-container')
+
 const validateInput = () =>  inputElement.value.trim().length > 0;
 
 const handleAddTask = () => {
@@ -10,7 +12,51 @@ const handleAddTask = () => {
      return inputElement.classList.add("error");
     }
 
+    const taskItemContainer = document.createElement('div');
+    taskItemContainer.classList.add('task-item');
 
+    const taskContent = document.createElement('p');
+    taskContent.innerText = inputElement.value;
+
+    taskContent.addEventListener('click', () => handleclick())
+
+    const deleteItem = document.createElement("i")
+    deleteItem.classList.add("far");
+    deleteItem.classList.add("fa-trash-alt");
+
+    deleteItem.addEventListener('click', () => 
+        handleDeleteClick(taskItemContainer, taskContent))
+
+    taskItemContainer.appendChild(taskContent)
+    taskItemContainer.appendChild(deleteItem)
+
+    taskContainer.appendChild(taskItemContainer);
+
+    inputElement.value = "";
+};
+
+const handleclick = (taskContent) => {
+    const tasks = taskContainer.childNodes;
+
+    for (const task of tasks) {
+        const currentTaskIsBeingClicked = task.firstChild.isSameNode(taskContent)
+
+        if (currentTaskIsBeingClicked) {
+            task.firstChild.classList.toggle("completed");
+        }
+    }
+};
+
+const handleDeleteClick = (taskItemContainer, taskContent) => {
+    const tasks = taskContainer.childNodes;
+
+    for (const task of tasks) {
+        const currentTaskIsBeingClicked = task.firstChild.isSameNode(taskContent)
+
+        if (currentTaskIsBeingClicked) {
+            taskItemContainer.remove();
+        }
+    }
 };
 
 const handleInputChange = () => {
@@ -21,6 +67,6 @@ const handleInputChange = () => {
     }
 }
 
-addTaskButton.addEventListener('click', () => handleAddTask());
+addTaskButton.addEventListener("click", () => handleAddTask());
 
 inputElement.addEventListener("change", () => handleInputChange())
